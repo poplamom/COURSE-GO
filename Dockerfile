78b89020c66bb7bf9db156bcb0bb195250650032
poplamom <poplamom@gmail.com>
2021-09-05 22:26:42 +0700

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:1.16-alpine
+RUN mkdir /app
+ADD . /app
+WORKDIR /app
+RUN go mod download
+RUN go build -o main .
+CMD ["/app/main"]
+
+# docker build -t goapi .
+# docker image ls
+# docker run --name goapi -p 5200:5200 goapi
+# docker tag goapi msitproject.azurecr.io/goapi
+# docker push msitproject.azurecr.io/goapi
--- controllers/controller.go
+++ controllers/controller.go
@@ -24,19 +24,25 @@ type pagination struct{
 }
 
 func (p *pagination) paginate() *pagingResult{
+
+	// Get limit
 	page, _ := strconv.Atoi(p.ctx.DefaultQuery("page","1"))
 	limit, _ := strconv.Atoi(p.ctx.DefaultQuery("limit","12"))
 
 
+	// Count records
 	ch := make(chan int)
 	go p.countRecords(ch)
 
+	// Find Record
 	offset := (page - 1) * limit
 	p.query.Limit(limit).Offset(offset).Find(p.records)
 	
+	// Total page
 	count := <- ch
 	totalPage := int(math.Ceil(float64(count) / float64(limit)))
 
+	// Find nextPage
 	var nextPage int
 	if page == totalPage {
 		nextPage = totalPage
@@ -44,6 +50,7 @@ func (p *pagination) paginate() *pagingResult{
 		nextPage = page - 1
 	}
 
+	// Result
 	return &pagingResult{
 		Page: page,
 		Limit: limit,
